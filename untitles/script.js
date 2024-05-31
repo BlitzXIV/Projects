b9607eba1ba770d0e8d7c54adf3400c9bcef3b69
Max <maxmadera04@gmail.com>
2024-05-30 21:38:19 -0400

    .

--- untitles/script.js
+++ untitles/script.js
@@ -1,11 +1,19 @@
-function listItem(){
-    document.getElementById('form').addEventListener('submit')
+const inpt = document.getElementById('item');
+const btn = document.getElementById('button');
+const todo = document.getElementById('todolist'); 
+let counter = 0;
 
-   let get = document.getElementById('name').value;
-   
-   const itemDetails = document.createElement('p')
-   itemDetails.textContent = ${name}
-   document.getElementsByClassName('todolist').appendChild(itemDetails);
-   
-   
-}
+    form.addEventListener('submit',function(event){
+        event.preventDefault();
+        
+        if(counter != 10){
+        const todoMsg = document.createElement('p');
+        todoMsg.textContent = inpt.value; 
+        todo.appendChild(todoMsg);
+        counter += 1; 
+        } else{alert("You have reached the maximum number of to-do items.");}
+         else{inpt.reportValidity};
+    });
+
+
+
