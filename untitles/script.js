a563ac1336a9881ea4024bc6567571e4be919760
Max <maxmadera04@gmail.com>
2024-05-31 19:37:54 -0400

    .

--- untitles/script.js
+++ untitles/script.js
@@ -5,20 +5,36 @@ let counter = 0;
 
     form.addEventListener('submit',function(event){
         event.preventDefault();
-        
+
         if (inpt.checkValidity()){
             if(counter != 20){
             const todoMsg = document.createElement('p');
             const delBtn = document.createElement('button');
+            const editBtn = document.createElement('button');
             todoMsg.textContent = inpt.value; 
             delBtn.textContent = 'Delete'
+            editBtn.textContent = 'Edit'
                 
             delBtn.addEventListener('click', function(){
                 todo.removeChild(todoMsg);
                 counter -= 1;
             });
 
+            editBtn.addEventListener('click', function(){
+                const editBox = document.createElement('input');
+                editBox.type = 'text';
+                editBox.value = todoMsg.firstChild.textContent;
+                
+                const saveBtn = document.createElement('button');
+                saveBtn.textContent = 'Save';
+                saveBtn.addEventListener('click', function(){ 
+                    todoMsg.textContent = editBox.value;
+                    
+                })
+                
+            })
             todoMsg.appendChild(delBtn);
+            todoMsg.appendChild(editBtn);
             todo.appendChild(todoMsg);
             counter += 1; 
             inpt.value = '';
@@ -28,4 +44,4 @@ let counter = 0;
     });
 
 
-// create delete button, link to list item, delete item 
+
